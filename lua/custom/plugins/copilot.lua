90b55d1152a66dfc9c20eaf9ce63fa0eb3614ac1
mattblackford <mzb@phoebussoftware.com>
2024-06-25 10:44:11 +0100

    Reset

--- lua/custom/plugins/copilot.lua
+++
@@ -1,6 +0,0 @@
-return {
-	'github/copilot.vim',
-	setup = function()
-		vim.g.copilot_assume_mapped = true
-	end,
-}
--- lua/custom/plugins/theme.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"doums/darcula",
-	name = "darcula",
-	lazy = false,
-	config = function()
-		vim.cmd("colorscheme darcula")
-	end
-}
